58d7023b0575378ddd9fd995b08ab1c328ce80c3
dqarias <dino.quispea@gmail.com>
2023-03-15 23:39:40 -0500

    Add service for fetching data

--- src/services/PokeApiService.js
+++ src/services/PokeApiService.js
@@ -1,10 +1,19 @@
 import axios from "axios";
 
-class PokeApiService {
+export default class PokeApiService {
 
     constructor(){
-
+        this.api = axios.create({
+            baseURL: 'https://pokeapi.co/api/v2/'
+        })
     }
 
-    
+    getPokemon = async () => {
+        try{
+            const { data } = await this.api.get('pokemon');
+            return data;
+        }catch(error){
+            return []
+        }
+    }
 }
